8f4345d732bfdf2c7260ac0d600837a47fab1591
mkalin <kalin@cs.depaul.edu>
2017-01-26 22:26:28 -0600

    update

--- dlock/Deadlock.java
+++ dlock/Deadlock.java
@@ -1,20 +1,20 @@
 /**
-   A program to illustrate deadlock. Here's a summary of what happens
-   when the program runs:
+   A program to illustrate deadlock. Here's a summary of what happens:
 
    -- Two Friend instances are created, with references lou (name Lou) and bud (name Bud).
 
-   -- Lou bows to Bud by way of greeting, and then Bud tries to bow back to Lou.
+   -- Lou bows to Bud by way of greeting, so Bud should bow back to Lou.
 
    -- The Friend class has two synchronized methods, bow and bowBack. The implicit lock is 'this',
-      a reference to the particular friend.
+      a reference to the particular friend -- in this case, Lou or Bud.
 
    -- The bowBack method is invoked within the bow method; but the lock ensures mutual exclusion so
-      that only _either_ bow _or_ bowBack can be invoked, not both at once by invoking one within
-      the other.
+      that only _either_ bow _or_ bowBack can be run at a given time, not both at once.
 
    -- The result is deadlock: the call to bowBack, the last statement in bow, must wait until bow
       exits -- but bow can't exit until bowBack exits, and bowBack can't even begin until bow exits.
+
+   -- The same thing happens when Bud bows to Lou.
 
    -- Yikes!
  */
@@ -29,12 +29,14 @@ public class Deadlock {
             return this.name;
         }
 
+	// Only one of 'bow' and 'bowBack' can execute at a time, as they
+	// are both locked on 'this' -- the current Friend instance.
         public synchronized void bow(Friend bower) {
             System.out.format("%s: %s"
 			      + "  has bowed to me!%n", 
 			      this.name, bower.getName());
-            bower.bowBack(this);
-        }
+            bower.bowBack(this); // executing bowBack would violate mutual exclusion
+        } // lock is released only here
 
         public synchronized void bowBack(Friend bower) {
             System.out.format("%s: %s"
@@ -43,7 +45,7 @@ public class Deadlock {
         }
     }
     
-    public static void main(String[] args) {
+    public static void main(String[ ] args) {
         final Friend lou = new Friend("Lou");
         final Friend bud = new Friend("Bud");
 
