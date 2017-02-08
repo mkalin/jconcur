23a80b3f4afded0fd90128d56d8827da601dc161
mkalin <kalin@cs.depaul.edu>
2017-02-07 20:05:27 -0600

    update

---
+++ basics/MultithreadEx.java
@@ -0,0 +1,51 @@
+import java.awt.Frame; // top-level window
+import java.util.Set;
+
+public class MultithreadEx {
+    public static void main(String[ ] args) {
+	new MultithreadEx().demo();
+	System.out.println("\nmain-thread is about to exit...");
+    } //### main-thread terminates when it exits main()
+
+    private void demo() {
+
+	listThreads("Before Frame is shown...\n");
+
+	// a new thread manages the Frame when the Frame is shown
+	new Frame("Just a demo, folks").setVisible(true);
+
+	listThreads("\nAfter Frame is shown...\n");
+    } 
+
+    private void listThreads(String msg) {
+	System.out.println(msg);
+
+	Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+	for (Thread t : threadSet) 
+	    System.out.printf("\tName, Id, isDaemon(): %s (%d) %b\n",
+			      t.getName(), t.getId(), t.isDaemon());
+    }
+}
+
+/** 
+    Output from a sample run:
+
+    Before Frame is shown...
+    
+        Name, Id, isDaemon(): Reference Handler (2) true
+	Name, Id, isDaemon(): Signal Dispatcher (4) true
+	Name, Id, isDaemon(): Finalizer (3) true
+	Name, Id, isDaemon(): main (1) false                       ### User thread
+
+    After Frame is shown...
+
+	Name, Id, isDaemon(): Reference Handler (2) true
+	Name, Id, isDaemon(): Java2D Disposer (10) true
+	Name, Id, isDaemon(): Signal Dispatcher (4) true
+	Name, Id, isDaemon(): Finalizer (3) true
+	Name, Id, isDaemon(): main (1) false                       ### User thread
+	Name, Id, isDaemon(): AWT-Shutdown (13) false              ### User thread
+	Name, Id, isDaemon(): AWT-XAWT (12) true
+
+    main-thread is about to exit...
+*/
