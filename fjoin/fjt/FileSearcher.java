1ea02e12bc5080a6d017e49ade3f49dab6db9dbb
mkalin <kalin@cs.depaul.edu>
2017-02-21 00:54:00 -0600

    update

--- fjoin/fjt/FileSearcher.java
+++ fjoin/fjt/FileSearcher.java
@@ -28,11 +28,21 @@ import java.util.concurrent.RecursiveTask;
  * extension (in this case, ".java"), then the file is added to the list of 
  * found files: this is a base case, and so no new task is generated.
  *
+ * Java's Fork/Join framework is a high-level API for the 'scatter/gather' idiom:
+ * scatter ('fork') the tasks to be handled among different threads, then gather ('join') 
+ * the results together.
+ *
  */
-public class FileSearcher extends RecursiveTask<List<String>> {
-    private final String path;
-    private final String extension;
+
+// The List<String> in RecursiveTask is a list of file names with a desired extension.
+// A RecursiveTask is one that can divided into subtasks of the same kind. In this case,
+// the RecursiveTask is to search a tree--a hierarchical file system--because the subtrees are
+// themselves (child) file systems.
+public class FileSearcher extends RecursiveTask<List<String>> { 
+    private final String path;        // where to start a task/subtask
+    private final String extension;   // what kind of file to look for
  
+    // a FileSearcher needs a starting path and a desired file extension.
     public FileSearcher(String path, String extension) {
 	this.path = path;
 	this.extension = extension;
@@ -42,29 +52,35 @@ public class FileSearcher extends RecursiveTask<List<String>> {
     public List<String> compute() {
 	List<String> listOfFileNames = new ArrayList<String>(); // empty list
 
+	// files in the current path (including subpaths)
 	File[ ] files = new File(path).listFiles();
 	if (files == null) return listOfFileNames; // base case
 
+	// Iterate through the files, keeping a list of the tasks generated
+	// to search the directories among the files.
 	List<FileSearcher> tasks = new ArrayList<FileSearcher>();
 	for (File file : files) {
 	    String absolutePath = file.getAbsolutePath();
 
-	    if (file.isDirectory()) { // a directory is the recursive case
+	    // Recursive case: a directory is a collection of files
+	    if (file.isDirectory()) { 
 		FileSearcher task = new FileSearcher(absolutePath, extension);
-		task.fork();     // recursive case
-		tasks.add(task); // keep track of the new task
+		task.fork();     // new task to search the directory
+		tasks.add(task); // keep track of the new task for a report
 	    }
+	    // Base case: a non-directory file -- with the desired extension?
 	    else if (file.getName().endsWith(extension))
 		listOfFileNames.add(absolutePath); // keep track of the found files
 	}
 
+	// Gather results from the subtasks.
 	assembleResults(listOfFileNames, tasks);
 	return listOfFileNames;
     }
 
     private void assembleResults(List<String> list, List<FileSearcher> tasks) {
 	for (FileSearcher task : tasks) {
-	    Collection<String> results = task.join();
+	    Collection<String> results = task.join(); // the 'join' in fork-join
 	    list.addAll(results);
 	}
     }
