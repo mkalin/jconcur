dcb5d0aa848f46abdde4105abb73f95589f518a6
mkalin <kalin@cs.depaul.edu>
2017-03-14 13:28:11 -0500

    update

--- class1Afternoon/dataRace/atomicC/AccountAtomicC.java
+++ class1Afternoon/dataRace/atomicC/AccountAtomicC.java
@@ -1,11 +1,18 @@
 package atomicC;
 
-// ## Of particular interest in this example is that the methods
-// incrementAndGet() and decrementAndGet() are synchronized on the _same_
-// lock, a particular AtomicInteger instance, in this case
-// AtomicInteger.balance: in effect, then, the two methods are
-// synchronized on 'this'. The result is that only one of these methods can
-// execute at a time on the balance object -- mutual exclusion is thereby achieved.
+/**
+ * Of particular interest in this example is that the methods
+ * incrementAndGet() and decrementAndGet() are synchronized on the _same_
+ * lock, a particular AtomicInteger instance, in this case
+ * AtomicInteger.balance: in effect, then, the two methods are
+ * synchronized on 'this'. The result is that only one of these methods can
+ * execute at a time on the balance object -- mutual exclusion is thereby achieved.
+ *
+ * The 'atomic' in the package name signals that operations such as 
+ * incrementing and decrementing occur 'atomically', that is, without interruption.
+ * For example, a thread that increments an AtomicInteger cannot be interrupted 'half way'
+ * through, thereby raising the specter of a race condition.
+ */
 import java.util.concurrent.atomic.AtomicInteger; // thread-safety is built-in
 
 class Miser extends Thread {       // deposit
