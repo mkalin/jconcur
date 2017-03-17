0f86b5352142aca2576ae462c786017068bcc2ee
mkalin <kalin@cs.depaul.edu>
2017-03-17 15:42:25 -0500

    update

--- class2Morning/tpool/ThreadPool.java
+++ class2Morning/tpool/ThreadPool.java
@@ -1,36 +1,32 @@
 package tpool;
 
 /** 
-    Usage:
-
-    Any job submitted to the thread pool must implement Runnable by defining the run() method:
-
-    public MyJob implements Runnable { 
-        public MyJob(String name) { ... }
-
-        @Override
-	public void run() { ... } // run implements whatever logic the job requires
-        ...
-    }
-
-    A job may be submitted for exeucution to a thread pool of N threads:
-
-    MyJob job1 = new MyJob("play a game");
-    MyJob job2 = new MyJob("compress the database");
-    ...
-    int howMany = 8;     // 8 threads in the pool
-
-    ThreadPool tpool = new ThreadPool(howMany);
-    tpool.execute(job1); // the tpool now provides the thread
-    tpool.execute(job2); // ditto
-    ...
+ *    Any job submitted to the thread pool must implement Runnable by defining the run() method:
+ *
+ *    public MyJob implements Runnable { 
+ *       public MyJob(String name) { ... }
+ *
+ *       @Override
+ *  	 public void run() { ... } // run implements whatever logic the job requires
+ *       ...
+ *    }
+ *
+ *    A job may be submitted for exeucution to a thread pool of N threads:
+ *
+ *    MyJob job1 = new MyJob("play a game");
+ *    MyJob job2 = new MyJob("compress the database");
+ *    ...
+ *    ThreadPool tpool = new ThreadPool(8); // 8 threads in the pool
+ *    tpool.execute(job1); // the tpool now provides the thread
+ *    tpool.execute(job2); // ditto
+ *    ...
 */
 import java.util.LinkedList;
 
 final public class ThreadPool {
-    private final int threadCount;
-    private final PoolWorker[ ] poolWorkers;
-    private final LinkedList<Runnable> jobQueue;
+    private final int threadCount;               // size of thread pool
+    private final PoolWorker[ ] poolWorkers;     // threads to do the work
+    private final LinkedList<Runnable> jobQueue; // the work to be done
 
     public ThreadPool(int n) {
         jobQueue = new LinkedList<Runnable>();
@@ -47,7 +43,7 @@ final public class ThreadPool {
     // API method: a ThreadPool client creates a pool, and then 
     // invokes execute on a job to be run.
     public void execute(Runnable job) {
-        synchronized(jobQueue) {
+        synchronized(jobQueue) {   // synchronized to prevent simultaneous adds/removes from queue
             jobQueue.addLast(job); // add the job to the end of the queue
             jobQueue.notify();     // wake up a thread to handle the job at the start of the queue
         }
@@ -61,6 +57,9 @@ final public class ThreadPool {
 	@Override
         public void run() {
             Runnable job = null;
+
+	    // A thread runs indefinitely within this 'infinite loop', waiting or
+	    // working, as appropriate.
             while (true) {
 		// The synchronization ensures that jobs are not simultaneously
 		// added to and removed from the job queue. If there are no pending
@@ -80,7 +79,7 @@ final public class ThreadPool {
                     job.run(); // run the job (outside the synchronized block)
                 }
                 catch (RuntimeException e) { }
-            }
+            } // while (true)
         }
     }
 }
