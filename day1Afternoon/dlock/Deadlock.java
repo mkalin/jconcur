61f91ce469e99683c3b2c999653eb64129c5b932
mkalin <kalin@cs.depaul.edu>
2017-05-02 01:25:13 -0500

    update

--- day1Afternoon/dlock/Deadlock.java
+++ day1Afternoon/dlock/Deadlock.java
@@ -1,24 +1,44 @@
 package dlock;
 
 /**
-   A program to illustrate deadlock. Here's a summary of what happens:
+   A program to illustrate deadlock. Here's a scenario in which deadlock can occur, with
+   the clock ticks numbered T0, T1,..., and Bud and Lou as two friends who bow, and return a
+   bow, as a formal greeting. Let the thread that represents Lou be TL, and the thread
+   representing Bud be TB. 
 
-   -- Two Friend instances are created, with references lou (name Lou) and bud (name Bud).
+   In each Friend instance, the bow(...) and bowBack(...) methods are synchronized
+   on the Friend instance; hence, invoking bow(...) on the instance precludes
+   calling bowBack(...) on that instance -- until bow(...) has exited, thereby
+   releasing the lock on bowBack(...).
 
-   -- Lou bows to Bud by way of greeting, so Bud should bow back to Lou.
+   Here's a scenario that likely results in deadlock. The scenario is likely, but not certain: if
+   you run the program, the program probably will deadlock. 
 
-   -- The Friend class has two synchronized methods, bow and bowBack. The implicit lock is 'this',
-      a reference to the particular friend -- in this case, Lou or Bud.
+   T0: Thread TL, which the program starts first, execute's Lou's bow(...) method (with Bud as
+       the argument), which thereby locks Lou's bowBack(...) method as well.
 
-   -- The bowBack method is invoked within the bow method; but the mutex ensures mutual exclusion so
-      that only either bow or bowBack can be run at a given time, not both at once.
+   T1: TL has not yet exited Lou's bow(...) method, and thread TB executes Bud's bow(...) method, 
+       with Lou as the argument. Bud's bowBack(...) method is now locked as well.
 
-   -- The result is deadlock: the call to bowBack, the last statement in bow, must wait until bow
-      exits -- but bow can't exit until bowBack exits, and bowBack can't even begin until bow exits.
+   T2: TB has not yet exited Bud's bow(...) method, and thread TL tries to execute 
+       Bud's bowBack(...) method -- but this method is locked because TB has not yet exited
+       Bud's bow(...) method, which alone releases the lock.
 
-   -- The same thing happens when Bud bows to Lou.
+   T3: Thread TB tries to execute Lou's bowBack(...) method, but it's locked as well because
+       TL has not yet exited Lou's bow(...) method.
 
-   -- Yikes!
+   At this point, thread TL is waiting for thread TB to release the lock on Bud's bowBack(...),
+   but this release can occur only if thread TL can execute Bud's locked bowBack(...) method. The
+   situation is the same with respect to TB: this thread is waiting for TL to release its
+   lock on Lou's bowBack(...) method. 
+
+   In summary, each thread now awaits the release of a lock, but such a release can occur only
+   if each thread proceeds. The result is deadlock. Here's the likely output:
+
+      Lou bows to Bud...
+      Bud bows to Lou...
+
+   Now the program needs to be terminated with the trusty Control-C from the command-line prompt.
  */
 public class Deadlock {
     static class Friend {
@@ -34,15 +54,13 @@ public class Deadlock {
 	// Only one of 'bow' and 'bowBack' can execute at a time, as they
 	// are both locked on 'this' -- the current Friend instance.
         public synchronized void bow(Friend bower) {
-            System.out.format("%s: %s"
-			      + "  has bowed to me!\n", 
+            System.out.format("%s bows to %s...\n",
 			      this.getName(), bower.getName());
             bower.bowBack(this);          //### executing bowBack would violate mutual exclusion
         } // lock is released only here
 
         public synchronized void bowBack(Friend bower) {
-            System.out.format("%s: %s"
-			      + " has bowed back to me!\n",
+            System.out.format("%s bows back to %s...\n",
 			      this.getName(), bower.getName());
         }
     }
